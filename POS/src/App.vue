465e8c007e5d6a97e5051496e5ee62f0f65639d3
caiot0 <caiotoscanob@gmail.com>
2026-02-14 15:52:18 -0300

    Add POSFooter component to App.vue

--- POS/src/App.vue
+++ POS/src/App.vue
@@ -2,10 +2,12 @@
   <div>
     <router-view :key="translationVersion" />
     <Toast />
+    <POSFooter />
   </div>
 </template>
 
 <script setup>
 import Toast from "@/components/common/Toast.vue"
+import POSFooter from "@/components/common/POSFooter.vue"
 import { translationVersion } from "@/utils/translation"
 </script>
